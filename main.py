c01bb51ae3fae541cf195f7b34307656a850ae5d
Itteba Gilani <itteba1@gmail.com>
2024-04-27 20:55:42 -0400

    final

--- main.py
+++ main.py
@@ -43,3 +43,44 @@ for i, sentence in enumerate(sentences):
     y[i, char_to_index[next_char[i]]] = 1
 '''
 model = tf.keras.models.load_model('poetry.h5')
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def generate_text(length, temperature):
+    start_index = random.randint(0, len(text) - SEQ_LENGTH - 1)
+    generated = ''
+    sentence = text[start_index: start_index+SEQ_LENGTH]
+    generated += sentence
+
+    for i in range(length):
+        x = np.zeros((1, SEQ_LENGTH, len(characters)))
+        
+        for t, character in enumerate(sentence):
+            x[0, t, char_to_index[character]] = 1
+
+        predictions = model.predict(x, verbose=0)[0]
+        next_index = sample(predictions, temperature)
+        next_character = index_to_char[next_index]
+
+        generated += next_character
+        sentence = sentence[1:] + next_character
+    
+    return generated
+
+
+print("----------0.2---------")
+print(generate_text(300, 0.2))
+print("----------0.4---------")
+print(generate_text(300, 0.4))
+print("----------0.6---------")
+print(generate_text(300, 0.6))
+print("----------0.8---------")
+print(generate_text(300, 0.8))
+print("----------1.0---------")
+print(generate_text(300, 1.0))
